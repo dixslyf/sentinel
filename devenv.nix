2c1250095bcee63735a3be6a4f877be2503efbed
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-10-02 13:19:07 +0800

    feat(devenv): init and replace flake
    Switch to `devenv` for local developer environment.
    So far it's really nice and more convenient than flakes.

---
+++ devenv.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = with pkgs; [ git ];
+
+  languages.python = {
+    enable = true;
+    package = pkgs.python312;
+    poetry = {
+      enable = true;
+      install = {
+        enable = true;
+        installRootPackage = true;
+      };
+      activate.enable = true;
+    };
+  };
+}
