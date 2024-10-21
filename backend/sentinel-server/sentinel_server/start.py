72c157588d17e79996dd402146d4337a4d0c0c9b
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-10-21 12:43:22 +0800

    refactor(sentinel-server): read environment variables instead of parsing CLI args
    Also, the server can now be started using FastAPI's CLI.

--- backend/sentinel-server/sentinel_server/start.py
+++ backend/sentinel-server/sentinel_server/start.py
@@ -1,10 +1,8 @@
 import logging
 import os
 import sys
-from argparse import ArgumentParser, Namespace
 
 import platformdirs
-import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from tortoise import Tortoise
@@ -13,38 +11,31 @@ import sentinel_server.auth
 import sentinel_server.config
 from sentinel_server.plugins import discover_plugins, load_plugins
 
-args: Namespace
 app = FastAPI()
-
-
-def parse_args() -> Namespace:
-    parser = ArgumentParser(prog="Sentinel")
-
-    parser.add_argument(
-        "--config-path",
-        help="Path to the configuration file",
-        default=os.path.join(platformdirs.user_config_dir("sentinel"), "config.toml"),
-    )
-
-    parser.add_argument(
-        "--log-level",
-        "-l",
-        default="info",
-        choices=["debug", "info", "warning", "error", "critical"],
-        help="Set the logging level",
-    )
-
-    return parser.parse_args()
 
 
 @app.on_event("startup")
 async def startup_event():
+    # Configure logging.
+    log_level = os.environ.get("SENTINEL_LOG_LEVEL", "INFO").upper()
+    if log_level not in ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"):
+        log_level = "NOTSET"
+
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=getattr(logging, log_level),
+        format="[%(levelname)s][%(asctime)s] %(message)s",
+    )
+
     # Create configuration and data directories.
     os.makedirs(platformdirs.user_config_dir("sentinel"), exist_ok=True)
     os.makedirs(platformdirs.user_data_dir("sentinel"), exist_ok=True)
 
     # Load configuration from the configuration file.
-    config_path = os.environ.get("SENTINEL_CONFIG_PATH", args.config_path)
+    config_path = os.environ.get(
+        "SENTINEL_CONFIG_PATH",
+        os.path.join(platformdirs.user_config_dir("sentinel"), "config.toml"),
+    )
     config = sentinel_server.config.get_config(config_path)
 
     # Initialise database.
@@ -74,14 +65,6 @@ async def api_root():
 
 
 def entry() -> None:
-    global args
-    args = parse_args()
-
-    # Configure logging.
-    logging.basicConfig(
-        stream=sys.stdout,
-        level=getattr(logging, args.log_level.upper()),
-        format="[%(levelname)s][%(asctime)s] %(message)s",
-    )
+    import uvicorn
 
     uvicorn.run(app, host="127.0.0.1", port=8000)
