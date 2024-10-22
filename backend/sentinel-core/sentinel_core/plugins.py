2462217f04c9bc44a7e257264ca273223cd5bcfe
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-10-22 19:34:26 +0800

    refactor(core): redesign plugin interfaces

--- backend/sentinel-core/sentinel_core/plugins.py
+++ backend/sentinel-core/sentinel_core/plugins.py
@@ -1,39 +1,54 @@
+import dataclasses
 from abc import abstractmethod
-from collections.abc import Iterable
+from collections.abc import Sequence
+from enum import Enum
+from typing import Any, Callable, Optional, Protocol, Self
 
 from sentinel_core.alert import Subscriber
 from sentinel_core.video import VideoStream
 from sentinel_core.video.detect import Detector
 
 
-class Plugin:
-    def __init__(self):
-        pass
+@dataclasses.dataclass(frozen=True)
+class Choice:
+    display_name: str
+    value: Any
 
+    @classmethod
+    def from_string(cls, value: str) -> Self:
+        return cls(display_name=value, value=value)
+
+
+@dataclasses.dataclass
+class ComponentArgDescriptor[T]:
+    display_name: str
+    arg_name: str
+    option_type: T
+    required: bool
+    default: Optional[T] = None
+    choices: Optional[set[Choice]] = None
+    validator: Optional[Callable[[T], Optional[str]]] = None
+    transform: Optional[Callable[[T], Any]] = None
+
+
+class ComponentKind(Enum):
+    VideoStream = 0
+    Detector = 1
+    Subscriber = 2
+
+
+@dataclasses.dataclass
+class ComponentDescriptor[T: VideoStream | Detector | Subscriber]:
+    display_name: str
+    kind: ComponentKind
+    cls: type[T]
+    args: Sequence[ComponentArgDescriptor]
+
+
+class Plugin(Protocol):
     @property
     @abstractmethod
-    def name(self) -> str:
+    def components(self) -> Sequence[ComponentDescriptor]:
         """
-        Returns the name of the plugin.
-        """
-
-    @property
-    @abstractmethod
-    def video_stream_classes(self) -> Iterable[type[VideoStream]]:
-        """
-        Returns the video source classes provided by this plugin.
-        """
-
-    @property
-    @abstractmethod
-    def detector_classes(self) -> Iterable[type[Detector]]:
-        """
-        Returns the object detector classes provided by this plugin.
-        """
-
-    @property
-    @abstractmethod
-    def subscriber_classes(self) -> Iterable[type[Subscriber]]:
-        """
-        Returns the subscriber classes provided by this plugin.
+        Returns the components provided by this plugin.
         """
