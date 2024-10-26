21f744970a8ce5d1dd5e550c4f66292a44fb0cb6
keith <keithneo00@gmail.com>
2024-10-26 12:34:43 +0800

    refactor(__init__.py, dashboard, login): start design for the new navbar Added global style to remove nicegui default padding for modularity instead of defining in each page.
    Started the new design for the nav bar. Right now only have the top nav bar
    will add the left nav bar later.

--- packages/sentinel-server/sentinel_server/ui/__init__.py
+++ packages/sentinel-server/sentinel_server/ui/__init__.py
@@ -2,18 +2,29 @@ from nicegui import ui
 
 from sentinel_server.ui.login import logout_user
 
+# Make this function global for modularity 
+def add_global_style():
+    # This line is to remove nicegui default padding
+    ui.add_head_html("<style>.nicegui-content { padding: 0 !important; }</style>")
+
 
 def pages_shared():
     # TODO: How to make the drawer retain its open/closed state across pages?
-    with ui.left_drawer(fixed=False, value=False).classes("shadow-2xl") as left_drawer:
-        ui.button("Dashboard", on_click=lambda: ui.navigate.to("/dashboard"))
-        ui.button("Cameras", on_click=lambda: ui.navigate.to("/cameras"))
-        ui.button("Devices", on_click=lambda: ui.navigate.to("/devices"))
-        ui.button("Alerts", on_click=lambda: ui.navigate.to("/alerts"))
-        ui.button("Settings", on_click=lambda: ui.navigate.to("/settings"))
 
-        ui.button("Logout", on_click=logout_user)
+    #TODO
+    # left nav bar 
+    
+    # top nav bar 
+    with ui.element("div").classes("border-2 w-screen h-16 flex justify-between"):
 
-    with ui.header():
-        ui.button(on_click=lambda: left_drawer.toggle(), icon="menu")
-        ui.label("Sentinel")
+        ui.label("Sentinel Icon Here").classes("flex item-center text-2xl")
+        
+        with ui.element("div").classes("flex gap-2 space-x-8 space-x-reverse"):
+            # TODO 
+            # here should grab username and place into the label
+            ui.label("username here").classes("flex items-center text-xl text-gray-500")
+
+            with ui.element("div").classes("flex items-center"):
+                with ui.button(on_click=logout_user).classes("h-10 w-12 text-xl").props('flat'):
+                    ui.icon("logout").classes("text-gray-400")
+            
