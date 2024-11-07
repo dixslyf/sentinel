16b233a9e74ff47e0f8860758b5554b7344b5798
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-11-07 13:34:02 +0800

    refactor(server/ui): remove dead code

--- packages/sentinel-server/sentinel_server/ui/__init__.py
+++ packages/sentinel-server/sentinel_server/ui/__init__.py
@@ -7,6 +7,12 @@ from nicegui.events import ClickEventArguments
 from sentinel_server.ui.login import logout_user
 
 
+def add_global_style():
+    # Remove NiceGui default padding.
+    ui.add_head_html("<style>body {background-color: FFFDD0; }</style>")
+    ui.add_head_html("<style>.nicegui-content { padding: 0 !important; }</style>")
+
+
 class SharedPageLayout:
     CONTAINER_PADDING: int = 8
 
@@ -16,9 +22,7 @@ class SharedPageLayout:
         self._setup_controls()
 
     def _setup_view(self) -> None:
-        # Remove NiceGui's default padding.
-        ui.add_head_html("<style>body {background-color: FFFDD0; }</style>")
-        ui.add_head_html("<style>.nicegui-content { padding: 0 !important; }</style>")
+        add_global_style()
 
         self._setup_navbar()
         self._setup_drawer()
@@ -60,7 +64,9 @@ class SharedPageLayout:
             "background-color: #5b6078"
         ):
             self._buttons: dict[str, Button] = {
-                label: SharedPageLayout._make_navbar_button(label, icon, self._page_header) # <-- add self._page_header
+                label: SharedPageLayout._make_navbar_button(
+                    label, icon, self._page_header
+                )
                 for label, icon in labels_icons.items()
             }
 
@@ -74,13 +80,12 @@ class SharedPageLayout:
         for label, button in self._buttons.items():
             button.on_click(SharedPageLayout._make_navigation_function(label))
 
-
     @staticmethod
-    def _make_navbar_button(label: str, icon: str, page_header: str) -> Button: # <-- add page_header param
+    def _make_navbar_button(label: str, icon: str, page_header: str) -> Button:
         button_classes: str = "flex items-start w-full"
         button_props: str = "flat no-caps"
 
-        if label.lower() == page_header.lower(): # <-- if statement to check page header
+        if label.lower() == page_header.lower():
             button_classes += " bg-[#727894]"
 
         button = ui.button().classes(button_classes).props(button_props)
@@ -103,81 +108,3 @@ class SharedPageLayout:
 
     def __exit__(self, *args):
         self._main_container.__exit__(*args)
-
-
-# Make this function global for modularity
-def add_global_style():
-    # This line is to remove nicegui default padding
-    ui.add_head_html("<style>body {background-color: FFFDD0; }</style>")
-    ui.add_head_html("<style>.nicegui-content { padding: 0 !important; }</style>")
-
-
-def pages_shared():
-    # top nav bar
-    with ui.header().classes("bg-[#24273a] justify-between items-center shadow-2xl"):
-        # ui.label("Sentinel").classes("text-2xl text-gray-200")
-        ui.image("sentinel_server/ui/static/sentinel_logo.png").classes("h-18 w-16")
-
-        with ui.element("div").classes("flex gap-2"):
-            ui.label(f"{app.storage.user['username']}").classes(
-                "flex items-center text-xl text-[#cad3f5]"
-            )
-
-            with ui.element("div").classes():
-                with ui.button(on_click=logout_user).props("flat"):
-                    ui.icon("logout").classes("text-[#cad3f5]")
-
-    # left nav bar
-    with ui.left_drawer(bottom_corner=True).classes("gap-10").style(
-        "background-color: #5b6078"
-    ):
-
-        # ui.button("Dashboard", on_click=lambda: ui.navigate.to("/dashboard"))
-        with ui.button(on_click=lambda: ui.navigate.to("/dashboard")).classes(
-            "flex items-start w-full"
-        ).props("flat no-caps"):
-            with ui.element("div").classes(
-                "flex items-center gap-3 text-xl text-[#cad3f5]"
-            ):
-                ui.icon("dashboard")
-                ui.label("Dashboard")
-
-        # ui.button("Cameras", on_click=lambda: ui.navigate.to("/cameras"))
-        with ui.button(on_click=lambda: ui.navigate.to("/cameras")).classes(
-            "flex items-start w-full"
-        ).props("flat no-caps"):
-            with ui.element("div").classes(
-                "flex items-center gap-3 text-xl text-[#cad3f5]"
-            ):
-                ui.icon("camera_alt")
-                ui.label("Cameras")
-
-        # ui.button("Devices", on_click=lambda: ui.navigate.to("/devices"))
-        with ui.button(on_click=lambda: ui.navigate.to("/devices")).classes(
-            "flex items-start w-full"
-        ).props("flat no-caps"):
-            with ui.element("div").classes(
-                "flex items-center gap-3 text-xl text-[#cad3f5]"
-            ):
-                ui.icon("devices_other")
-                ui.label("Devices")
-
-        # ui.button("Alerts", on_click=lambda: ui.navigate.to("/alerts"))
-        with ui.button(on_click=lambda: ui.navigate.to("/alerts")).classes(
-            "flex items-start w-full"
-        ).props("flat no-caps"):
-            with ui.element("div").classes(
-                "flex items-center gap-3 text-xl text-[#cad3f5]"
-            ):
-                ui.icon("notification_important")
-                ui.label("Alerts")
-
-        # ui.button("Settings", on_click=lambda: ui.navigate.to("/settings"))
-        with ui.button(on_click=lambda: ui.navigate.to("/settings")).classes(
-            "flex items-start w-full"
-        ).props("flat no-caps"):
-            with ui.element("div").classes(
-                "flex items-center gap-3 text-xl text-[#cad3f5]"
-            ):
-                ui.icon("settings")
-                ui.label("Settings")
